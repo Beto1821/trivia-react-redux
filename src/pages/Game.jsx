6b5cb774e7a0a518c605e85fcc51797cd853c11d
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-06-28 18:20:47 -0300

    requisito 5 concluido

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 
 class Game extends React.Component {
   render() {
-    return (<div>Game Page</div>);
+    const { gravatarEmail, score, name } = this.props;
+    const hashEmail = md5(gravatarEmail.trim().toLowerCase()).toString();
+    return (
+      <div>
+        <header>
+          <img src={ `https://www.gravatar.com/avatar/${hashEmail}` } alt="user" data-testid="header-profile-picture" />
+          <p data-testid="header-player-name">{name}</p>
+          <p data-testid="header-score">{score}</p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default connect()(Game);
+const mapStateToProps = ({ player, login }) => ({
+  name: login.userName,
+  assertions: player.assertions,
+  score: player.score,
+  gravatarEmail: login.userEmail,
+});
+
+Game.propTypes = {
+  gravatarEmail: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Game);
