91486c08e54e35a5f93be84e6965a99aea1e67e7
Thiago Gasparini <thiago_engprod@outlook.com>
2022-06-29 18:14:01 -0300

    requisito 21

---
+++ src/tests/mocks/requestApi.js
@@ -0,0 +1,13 @@
+export const response = {
+  response_code: 0,
+  results: [
+    {
+      category: 'Entertainment: Video Games',
+      type: 'multiple',
+      difficulty: 'easy',
+      question: 'What is the first weapon you acquire in Half-Life?',
+      correct_answer: 'A crowbar',
+      incorrect_answers: ['A pistol', 'The H.E.V suit', 'Your fists'],
+    },
+  ],
+};
